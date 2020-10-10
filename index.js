39737700fe2fa32ae5875d2ea93ae6e48abc9675
sabovyan <abovyan.sargis90@gmail.com>
2020-10-10 18:01:16 +0400

    👷 build(public) move html and css files to the public folder

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require
-('express');
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const pug = require('pug');
 
+const list = JSON.parse(fs.readFileSync(path.join(__dirname, '/state/list.json'), 'utf-8'));
 
 
 const app = express();
 
-app.use('/assets', express.static('assets'))
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) => {
-  const list = JSON.parse(fs.readFileSync(path.join(__dirname, '/state/list.json'), 'utf-8'));
-  res.render(path.join(__dirname, '/view/index.pug'), {
+  res.render(path.join(__dirname, '/public/view/index.pug'), {
     list: list
   });
 })
 
+app.delete('/', (res, req) => {
+
+})
 
 app.listen(4000, () => console.log('server is running'))
