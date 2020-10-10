2cb1efcd3c2b7eb487f21cb9068edc7a8c2930b9
sabovyan <abovyan.sargis90@gmail.com>
2020-10-10 15:29:52 +0400

    💚ci(list) add static appearance of list

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require
 ('express');
 const path = require('path');
+const fs = require('fs');
+
 
 
 const app = express();
@@ -8,7 +10,11 @@ const app = express();
 app.use('/assets', express.static('assets'))
 
 app.get("/", (req, res) => {
-  res.render(path.join(__dirname, '/view/index.pug'));
+  const list = JSON.parse(fs.readFileSync(path.join(__dirname, '/state/list.json'), 'utf-8'));
+  res.render(path.join(__dirname, '/view/index.pug'), {
+    list: list
+  });
 })
+
 
 app.listen(4000, () => console.log('server is running'))
