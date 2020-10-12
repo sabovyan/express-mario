fdcbbf38a0c124b08d4656c39a91df52f42f82be
sabovyan <abovyan.sargis90@gmail.com>
2020-10-12 13:18:19 +0400

    👷‍♂️ build(cookie parser) : add cookie parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const pug = require('pug');
 const controller = require('./controllers/conotroller');
 const paths = require('./constants/paths');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -12,8 +13,7 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 controller(app);
-
-app.put('/', (req, res) => {});
 
 app.listen(4000, () => console.log('server is running'));
